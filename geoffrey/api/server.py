ce7f62551b33253d6ade77f09a3f558addf6c14e
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-11-08 18:16:24 -0600

    make geoffrey config aware

--- geoffrey/api/server.py
+++ geoffrey/api/server.py
@@ -1,14 +1,36 @@
+from werkzeug.exceptions import Unauthorized
 from klein import Klein
 from geoffrey import __version__
+from geoffrey import config
 
-app = Klein()
+
+class GeoffreyApi(Klein):
+
+    def _get_config(self, request):
+        """
+        Checks and returns the proper configuration for the request|
+
+        raises l{werkzeug.exceptions.Unauthorized} if the key given
+        in the request doesn't match the API_KEY specified in the
+        config
+        """
+        api_key = request.args.get("key", [None])[0]
+        if not api_key or config.API_KEY != api_key:
+            raise Unauthorized()
+        return config
+
+app = GeoffreyApi()
+
 
 @app.route('/ping')
 def ping(request):
-    return 'Hello, world!'
+    config = app._get_config(request)
+    return 'Your API key is:{}'.format(config.API_KEY)
 
 
 @app.route('/version')
 def version(request):
     return 'This runs Geoffrey %s' % __version__
+
+
 
