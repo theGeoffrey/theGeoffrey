0a136d84921af762d4aab63e65035602810f8e18
anoukruhaak <anoukruhaak@gmail.com>
2014-11-12 17:49:56 -0600

    adds add_user route to api server

--- geoffrey/api/server.py
+++ geoffrey/api/server.py
@@ -3,7 +3,7 @@ from twisted.internet import defer
 from klein import Klein
 from geoffrey import __version__
 from geoffrey.config import CONFIG
-
+import json
 
 class GeoffreyApi(Klein):
 
@@ -24,9 +24,18 @@ class GeoffreyApi(Klein):
     def secure(self, func):
         def secured(request):
             return self._get_config(request).addCallback(func)
+        secured.func_name = func.func_name
         return secured
 
 app = GeoffreyApi()
+
+
+@app.route('/users/add', methods=["POST"])
+@app.secure
+def add_user(request):
+    payload = json.loads(request.content.read())
+    return "Received User ID: {}".format(payload['user_id'])
+
 
 @app.route('/ping')
 @app.secure
@@ -37,6 +46,3 @@ def ping(request):
 @app.route('/version')
 def version(request):
     return 'This runs Geoffrey %s' % __version__
-
-
-
