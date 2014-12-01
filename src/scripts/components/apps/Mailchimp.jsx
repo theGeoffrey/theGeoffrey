6bbe948b2a84bd84478f6f0b655f014013966f90
anoukruhaak <anoukruhaak@gmail.com>
2014-11-30 18:42:06 -0600

    adds list_id and pretty design to mailchimp app

--- src/scripts/components/apps/Mailchimp.jsx
+++ src/scripts/components/apps/Mailchimp.jsx
@@ -10,29 +10,29 @@ var React = require('react/addons'),
 
 var Mailchimp = React.createClass({
   _key: 'mailchimp',
-  _sync_keys: ['api_key'],
+  _sync_keys: ['api_key', 'list_id'],
   _services: ['mailchimp_subscribe'],
   _name: "Mailchimp",
   mixins: [SimpleAppMixin],
   _render: function(){
     return(
         <form onSubmit={this._defaultFormSubmit}>
-          <dl>
-            <dt>Mailchimp API Key</dt>
-            <dd>
-              <input type="text" value={this.state.api_key}  placeholder="add key here" ref="api_key" />
-            </dd>
-              <dl>
-                <dt>
-                  <label>
-                    <input type="checkbox"
-                           defaultChecked={this.state.mailchimp_subscribe}
-                           ref="mailchimp_subscribe" />
-                    Subscribe new Users to Mailchimp automatically
-                  </label>
-                </dt>
-              </dl>
-          </dl>
+          <div className="form-group">
+             <label for="mcApiKey">Mailchimp API Key</label>
+             <input type="text" className="form-control" id="mcApiKey" value={this.state.api_key}  placeholder="add key here" ref="api_key" />
+          </div>
+          <div className="form-group">
+             <label for="mcListId">Mailchimp List ID</label>
+             <input type="text" className="form-control" id="mcListId" value={this.state.list_id}  placeholder="add list id here" ref="list_id" />
+          </div>
+          <div className="checkbox">
+          <label>
+            <input type="checkbox"
+                   defaultChecked={this.state.mailchimp_subscribe}
+                   ref="mailchimp_subscribe" />
+                   Subscribe new Users to Mailchimp automatically
+          </label>
+          </div>    
           <button className="btn btn-primary" type="submit">Save</button>
         </form>
     );
