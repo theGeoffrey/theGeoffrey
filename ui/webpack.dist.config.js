fbeb77084f0a73211ec242ce40e68e50a1b697df
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-01-24 01:47:53 +0000

    also match new woff2

--- ui/webpack.dist.config.js
+++ ui/webpack.dist.config.js
@@ -71,6 +71,9 @@ module.exports = {
       test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: "url-loader?limit=10000&minetype=application/font-woff"
     },{
+      test: /\.woff([0-9]*)$/,
+      loader: "url-loader?limit=10000&minetype=application/font-woff"
+    },{
       test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: "file-loader"
     }]
