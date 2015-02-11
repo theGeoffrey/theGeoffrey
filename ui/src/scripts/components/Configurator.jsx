fc461137a81a6fc75d7d19ae77129289b001c06d
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-02-11 18:08:21 +0000

    get api and public key from db configuration

--- ui/src/scripts/components/Configurator.jsx
+++ ui/src/scripts/components/Configurator.jsx
@@ -9,7 +9,8 @@ var React = require('react/addons'),
     AddApp = require('./AddApp'),
     AppPage = require('./AppPage'),
     Locations = Router.Locations,
-    MainConfig = require('./MainConfig')
+    MainConfig = require('./MainConfig'),
+    getDB = require("../stores/_db").getDB,
     Location = Router.Location;
 
 
@@ -21,9 +22,18 @@ var Configurator = React.createClass({
   },
 
   render: function() {
-    var showFullConfig = this.getPath() === '/';
+    var showFullConfig = this.getPath() === '/',
+        db_link = document.createElement("a");
+
+    // URL parsing
+    db_link.href = getDB()._db_name;
+
+    var public_key = (db_link.pathname || "/fake").slice(1),
+        api_key = db_link.username + ":" + db_link.password + "@" + public_key;
+
+    // console.log(db_link.host, db_link.hostname, db_link.username, db_link.password, db_link.pathname);
     return (<div>
-              <MainConfig showFull={showFullConfig} />
+              <MainConfig showFull={showFullConfig} public_key={public_key} api_key={api_key} />
               <Locations hash>
                 <Location path="/" handler={AppsOverview} />
                 <Location path="/apps/add" handler={AddApp} />
