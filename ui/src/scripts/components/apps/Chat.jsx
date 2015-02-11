00ca45692dc21e86964a90efb952c714f59cac0a
anoukruhaak <anoukruhaak@gmail.com>
2015-02-11 23:21:29 +0000

    adds rooms to chat admin panel

--- ui/src/scripts/components/apps/Chat.jsx
+++ ui/src/scripts/components/apps/Chat.jsx
@@ -6,6 +6,7 @@
 
 var React = require('react/addons'),
     SimpleAppMixin = require('./_mixin'),
+     _ = require('underscore'),
     Router = require('react-router-component'),
     keyChecker = require("../_helpers").keyChecker,
     rtbs = require('react-bootstrap'),
@@ -16,7 +17,34 @@ var React = require('react/addons'),
     stropheStore = require('../../chat/StropheStore'),
     initConnection = stropheStore.init,
     dispatcher = require('../../chat/dispatcher'),
+    actions = require('../../chat/actions'),
+    mucStore = require('../../chat/MucStore'),
+    ListGroup = rtbs.ListGroup,
+    ListGroupItem = rtbs.ListGroupItem,
+    rooms = mucStore.rooms,
+    fakerooms = ['oneRoom', 'twoRoom', 'threeRoom'],
     OAuth = window.OAuth;
+
+var RoomsList = React.createClass({
+
+  createRoom: function (){
+  actions.createRoom()
+  },
+  
+  render: function(){
+    return(
+      <div>
+      <ListGroup>
+      {_.map(fakerooms, function(name){
+              return(
+                 <ListGroupItem>{name}<button className="btn btn-primary">delete room</button></ListGroupItem>
+            )})}
+      </ListGroup>
+      <button className="btn btn-primary" onClick={this.createRoom}>Add Room</button>
+      </div>
+      );
+  }
+});
 
 var Chat = React.createClass({
   _color: COLOR,
@@ -26,7 +54,6 @@ var Chat = React.createClass({
   _services: ['chat_main', 'chat_shoutbox'],
   _name: "Chat",
   mixins: [SimpleAppMixin],
-
 
 connectToChatServer: function (e) {
     e.preventDefault();
@@ -49,9 +76,14 @@ connectToChatServer: function (e) {
   
   _render: function(){
     var server_mess = this.state.loading ? 'loading' : this.state.jid;
+    var message = <h4>Connect to views romms </h4>;
+    var show_rooms = this.state.loading ? message : <RoomsList />;
+  
     return(
+      <div>
         <form onSubmit={this._defaultFormSubmit} className='form-horizontal'>
-         <Input type="checkbox"
+          <h4>Activate Chat</h4>
+          <Input type="checkbox"
                    defaultChecked={this.state.chat_main}
                    ref="chat_main" 
                    wrapperClassName="col-xs-offset-1 col-xs-10"
@@ -63,13 +95,17 @@ connectToChatServer: function (e) {
                    label="Activate shoutbox"/> 
           <Input type='textarea' label="Server output:" labelClassName="col-xs-2" 
                       wrapperClassName="col-xs-10" value={server_mess}  
-                      placeholder="Groups" readOnly="true" ref="groups" /> 
-          <button className="btn btn-primary btn-form" onClick={this.connectToChatServer}>Connect</button>
+                      placeholder="Groups" readOnly ref="groups" /> 
           <button className="btn btn-primary btn-form" type="submit">Save</button>
         </form>
+
+         {show_rooms}
+        <button className="btn btn-primary btn-form" onClick={this.connectToChatServer}>Connect</button>
+      </div>
     );
   }
 });
+
 
 module.exports = {
         name: 'Chat',
